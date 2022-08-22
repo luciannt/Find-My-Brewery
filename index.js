9728912cdf7d1222e240828b4069695cbbbcb754
Jordan Jennings <jordanjennings12@gmail.com>
2022-08-22 16:16:36 -0400

    create brewery cards template

--- index.js
+++ index.js
@@ -14,10 +14,36 @@ function formHandler(city, state){
 
     //checks for city and state values
     if(city && state){
-    fetch(`https://api.openbrewerydb.org/breweries?by_city=${city}&by_state=${state}&per_page=6`)
-    .then(data => data.json())
-    .then(breweries => console.log(breweries))
+        fetch(`https://api.openbrewerydb.org/breweries?by_city=${city}&by_state=${state}&per_page=6`)
+        .then(data => data.json())
+        .then(breweries => {
+            breweries.forEach(brewery => {
+                console.log(brewery)
+                createBreweryCards(brewery)
+            });
+        })
+   
     } else (
-        alert("Please enter a state!")
+        alert("Please enter both city and state!")
     )
+}
+
+function createBreweryCards(brewery){
+    const li = document.createElement("li")
+    li.className = "brewery-item"
+    const h4 = document.createElement("h4")
+    const p = document.createElement("p")
+    const link = document.createElement("a")
+    const star = document.createElement("span")
+    star.setAttribute("id", "star");
+    h4.textContent = brewery.name;
+    p.textContent = `${brewery.street} ${brewery.city}, ${brewery.state} ${brewery.postal_code}`
+    link.href = brewery.website_url
+    link.textContent = "Visit website"
+    star.textContent = "☆"
+   
+    const breweryList = document.querySelector("#brewery-list")
+    li.append( h4, p, link, star)
+    breweryList.append(li)
+
 }
