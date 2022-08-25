01d0d06d8828e64da82c5999fac9990951d210cc
Jordan Jennings <jordanjennings12@gmail.com>
2022-08-25 12:10:46 -0400

    form changes

--- README.md
+++ README.md
@@ -7,17 +7,17 @@ Brew Hub is a one page app that uses a public API to search for breweries in a d
 
 The main functionality is built using a fetch request. The user has the ability to search by city and state, as well as type of brewery. The API returns up to 10 breweries with their name, address, and website. 
 
-![alt.text](./images/search.png)
+![this is an image](./images/search.png)
 
 
 The user is able to click the star ☆ to save that brewery to their favorites using a POST request. We are replicating a backend server by using json-server. The favorites are saved in the db.json file. Favorite breweries can be deleted as well by clicking the clicked star again.
 
 
-![alt.text](./images/favorites.png)
+![this is an image](./images/favorites.png)
 
 ## Try Brew Hub
 
-To test Brew Hub for yourself, clone this repository to your local computer. In the terminal run "json-server --watch db.json".
+To test Brew Hub for yourself, clone this repository to your local computer. In the terminal run ** **"json-server --watch db.json"**.
 
 To check out the API documentation: https://www.openbrewerydb.org/documentation
 
--- index.js
+++ index.js
@@ -162,26 +162,36 @@ function createFavCards(brewery){
 
 fetchFavorites(); 
 
+const subForm = document.getElementById('subscribe');
+console.log(subForm)
+
+subForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    validation();
+ })
+
 function validation() {  
-    let form = document.getElementById('form')  
     let email = document.getElementById('email').value  
     let text = document.getElementById('text')  
     let pattern = /^[^ ]+@[^ ]+.[a-z]{2,3}$/  
     if (email.match(pattern)) {  
-     form.classList.add('valid')  
-     form.classList.remove('invalid')  
+     subForm.classList.add('valid')  
+     subForm.classList.remove('invalid')  
      text.innerHTML = "Your Email Address in valid"  
      text.style.color = '#00ff00'  
     } else {  
-     form.classList.remove('valid')  
-     form.classList.add('invalid')  
+     subForm.classList.remove('valid')  
+     subForm.classList.add('invalid')  
      text.innerHTML = "Please Enter Valid Email Address"  
      text.style.color = '#ff0000'  
     }  
     if (email == '') {  
-     form.classList.remove('valid')  
-     form.classList.remove('invalid')  
+     subForm.classList.remove('valid')  
+     subForm.classList.remove('invalid')  
      text.innerHTML = ""  
      text.style.color = '#00ff00'  
+
+
     }  
    }  
+
